3792f55394a69f6bc0f15fa8d617c009d86a28c2
SeregaWorkNetbook <jobsfolder@yandex.ru>
2020-11-20 13:10:37 +0300

    added task about exceptions (task for Home Work 2 from Part 2)

---
+++ src/Second_2_Part/HW2_2_final/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package Second_2_Part.HW2_2_final;
+
+public class MyArraySizeException extends Throwable {
+
+    public MyArraySizeException() {
+        super("Тимофей, проверь по-скорее это задание ><\nЯ здесь делаю ошибки!");
+    }
+}
